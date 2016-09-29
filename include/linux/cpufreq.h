816e38c1425d902c473e93e6755554fd68b1cf2b
Trujulu <trujulu@gmail.com>
2016-09-29 19:20:56 +0200

    Add darkness, enable hotplug and interactiveplus governors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -424,6 +424,9 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_THUNDERX)
 extern struct cpufreq_governor cpufreq_gov_thunderx;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_thunderx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_darkness)
 #endif
 
 
