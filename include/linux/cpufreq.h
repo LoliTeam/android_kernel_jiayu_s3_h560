25c47bb59714d22edde5f564a8e6316353c1195c
Trujulu <trujulu@gmail.com>
2016-09-29 18:52:25 +0200

    Add Thunderx governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -421,6 +421,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_THUNDERX)
+extern struct cpufreq_governor cpufreq_gov_thunderx;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_thunderx)
 #endif
 
 
