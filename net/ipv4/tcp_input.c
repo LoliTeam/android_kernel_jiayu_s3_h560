6c805386d2ba9c3c3a5857696bd4015218614273
trujulu <trujulu@gmail.com>
2016-10-11 18:15:28 +0200

    Revert "Conflicts resolved"
    This reverts commit e21089a78cd66cf933e4f26a9e85fd68c2721a2e.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -68,7 +68,6 @@
 #include <linux/module.h>
 #include <linux/sysctl.h>
 #include <linux/kernel.h>
-#include <linux/reciprocal_div.h>
 #include <net/dst.h>
 #include <net/tcp.h>
 #include <net/inet_common.h>
@@ -3323,8 +3322,7 @@ static void tcp_send_challenge_ack(struct sock *sk)
 	/* unprotected vars, we dont care of overwrites */
 	static u32 challenge_timestamp;
 	static unsigned int challenge_count;
-	u32 now = jiffies / HZ;
-	u32 count;
+	u32 count, now;
 
 	/* Then check host-wide RFC 5961 rate limit. */
 	now = jiffies / HZ;
@@ -3332,13 +3330,12 @@ static void tcp_send_challenge_ack(struct sock *sk)
 		u32 half = (sysctl_tcp_challenge_ack_limit + 1) >> 1;
 
 		challenge_timestamp = now;
-		ACCESS_ONCE(challenge_count) = half +
-				  reciprocal_divide(prandom_u32(),
-					sysctl_tcp_challenge_ack_limit);
+		WRITE_ONCE(challenge_count, half +
+			   prandom_u32_max(sysctl_tcp_challenge_ack_limit));
 	}
-	count = ACCESS_ONCE(challenge_count);
+	count = READ_ONCE(challenge_count);
 	if (count > 0) {
-		ACCESS_ONCE(challenge_count) = count - 1;
+		WRITE_ONCE(challenge_count, count - 1);
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_TCPCHALLENGEACK);
 		tcp_send_ack(sk);
 	}
