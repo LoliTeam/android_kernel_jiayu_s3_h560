14441ac59d151981d31cd9e6fcc9c3cbfd531175
trujulu <trujulu@gmail.com>
2016-10-12 12:15:09 +0200

    Revert "fix compiling errors at headers"
    This reverts commit c797a8ded1927ab748debe05b3fefdc0c73af2dc.

--- drivers/of/fdt.c
+++ drivers/of/fdt.c
@@ -20,7 +20,6 @@
 #include <linux/string.h>
 #include <linux/errno.h>
 #include <linux/slab.h>
-#include <linux/libfdt.h>
 #include <linux/sysfs.h>
 #include <asm/setup.h>  /* for COMMAND_LINE_SIZE */
 #ifdef CONFIG_PPC
