89eacdc1ef5c828c9a1b2990c5173d2f64349493
Felipe de Jesús <felipealcocer30@gmail.com>
2016-10-27 20:30:30 -0500

    Update cust_battery_meter.h

--- drivers/misc/mediatek/mach/mt6752/h560/power/cust_battery_meter.h
+++ drivers/misc/mediatek/mach/mt6752/h560/power/cust_battery_meter.h
@@ -15,12 +15,13 @@
 //#define FIXED_TBAT_25
 
 /* ADC Channel Number */
+#if 0
 #define CUST_TABT_NUMBER 17
 #define VBAT_CHANNEL_NUMBER      7
 #define ISENSE_CHANNEL_NUMBER	 6
 #define VCHARGER_CHANNEL_NUMBER  4
 #define VBATTEMP_CHANNEL_NUMBER  5
-
+#endif
 /* ADC resistor  */
 #define R_BAT_SENSE 4					
 #define R_I_SENSE 4						
@@ -36,24 +37,24 @@
 #define FG_METER_RESISTANCE 	0
 
 /* Qmax for battery  */
-#define Q_MAX_POS_50	2950      
-#define Q_MAX_POS_25	2973	    
-#define Q_MAX_POS_0	2764     
-#define Q_MAX_NEG_10	2464	     
+#define Q_MAX_POS_50	3060
+#define Q_MAX_POS_25	3040
+#define Q_MAX_POS_0	2837
+#define Q_MAX_NEG_10	2702
 
-#define Q_MAX_POS_50_H_CURRENT	2874	      //2512
-#define Q_MAX_POS_25_H_CURRENT	2868	      //2453
-#define Q_MAX_POS_0_H_CURRENT		2524		      //2063
-#define Q_MAX_NEG_10_H_CURRENT	1840	      //893
+#define Q_MAX_POS_50_H_CURRENT	3012
+#define Q_MAX_POS_25_H_CURRENT	2953
+#define Q_MAX_POS_0_H_CURRENT		2563
+#define Q_MAX_NEG_10_H_CURRENT	1393
 
 
 /* Discharge Percentage */
-#define OAM_D5		 1		//  1 : D5,   0: D2
+#define OAM_D5		 0		//  1 : D5,   0: D2
 
 
 /* battery meter parameter */
 #define CHANGE_TRACKING_POINT
-#define CUST_TRACKING_POINT  14
+#define CUST_TRACKING_POINT  1
 #define CUST_R_SENSE         68
 #define CUST_HW_CC 		    0
 #define AGING_TUNING_VALUE   103
@@ -62,18 +63,18 @@
 #define OCV_BOARD_COMPESATE	0 //mV 
 #define R_FG_BOARD_BASE		1000
 #define R_FG_BOARD_SLOPE	1000 //slope
-#define CAR_TUNE_VALUE		94 //1.00
+#define CAR_TUNE_VALUE		102 //1.00
 
 
 /* HW Fuel gague  */
 #define CURRENT_DETECT_R_FG	10  //1mA
 #define MinErrorOffset       1000
 #define FG_VBAT_AVERAGE_SIZE 18
-#define R_FG_VALUE 			0 // mOhm, base is 20
+#define R_FG_VALUE 			10 // mOhm, base is 20
 
-#define CUST_POWERON_DELTA_CAPACITY_TOLRANCE	40
+#define CUST_POWERON_DELTA_CAPACITY_TOLRANCE	10
 #define CUST_POWERON_LOW_CAPACITY_TOLRANCE		5
-#define CUST_POWERON_MAX_VBAT_TOLRANCE			90
+#define CUST_POWERON_MAX_VBAT_TOLRANCE			94
 #define CUST_POWERON_DELTA_VBAT_TOLRANCE		30
 
 /* Disable Battery check for HQA */
@@ -84,7 +85,7 @@
 /* Dynamic change wake up period of battery thread when suspend*/
 #define VBAT_NORMAL_WAKEUP		3600		//3.6V
 #define VBAT_LOW_POWER_WAKEUP		3500		//3.5v
-#define NORMAL_WAKEUP_PERIOD		200 		//90 * 60 = 90 min
+#define NORMAL_WAKEUP_PERIOD		5400 		//90 * 60 = 90 min
 #define LOW_POWER_WAKEUP_PERIOD		300		//5 * 60 = 5 min
 #define CLOSE_POWEROFF_WAKEUP_PERIOD	30	//30 s
 
